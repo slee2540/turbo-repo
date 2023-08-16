441913e16d58fdd9bcc3b40f74e24e0653426ba1
slee2540 <slee2540.com>
2023-08-16 16:39:21 +0900

    add: react route and alias path

--- apps/vite-web/src/App.tsx
+++ apps/vite-web/src/App.tsx
@@ -1,12 +1,7 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-// import { Button, Header } from 'ui'
-
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-// import { SearchAddress } from './components'
+import { RickLayout } from '@layouts'
 import { Search } from './components'
 import './styles.css'
 
@@ -15,8 +10,13 @@ const queryClient = new QueryClient()
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Search />
-      {/* <SearchAddress /> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Search />} />
+          <Route path="/risk" element={<RickLayout />}></Route>
+          {/* <Route path="/risk" element={<>asdf</>}></Route> */}
+        </Routes>
+      </BrowserRouter>
       {/* The rest of your application */}
       <ReactQueryDevtools initialIsOpen />
     </QueryClientProvider>
--- apps/vite-web/vite.config.js
+++ apps/vite-web/vite.config.js
@@ -1,12 +1,35 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import analyze from 'rollup-plugin-analyzer';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import analyze from 'rollup-plugin-analyzer'
+import path from 'path'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    build: {
-        rollupOptions: {
-            plugins: [analyze()],
-        },
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      plugins: [analyze()],
     },
-});
+  },
+  resolve: {
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, 'src') },
+      // {
+      //   find: '@components',
+      //   replacement: path.resolve(__dirname, 'src/components'),
+      // },
+      // {
+      //   find: '@components',
+      //   replacement: path.resolve(__dirname, 'src/components/*'),
+      // },
+      {
+        find: '@layouts',
+        replacement: path.resolve(__dirname, 'src/layouts'),
+      },
+      // {
+      //   find: '@layouts',
+      //   replacement: path.resolve(__dirname, 'src/layouts/*'),
+      // },
+    ],
+  },
+})
